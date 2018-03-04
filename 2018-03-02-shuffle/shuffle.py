47bfc8148b52e6fee8d0d5a22f37ea6c7c688fe3
SCappella <>
2018-03-03 18:27:19 -0700

    Add local maximization

--- 2018-03-02-shuffle/shuffle.py
+++ 2018-03-02-shuffle/shuffle.py
@@ -38,3 +38,33 @@ def prob_max(num):
         if shuffles > max_shuffles:
             max_shuffles = shuffles
             print(deck, shuffles)
+
+
+def mini_shuffle(deck):
+    """Iterate through permutations that are one transposition away"""
+    for i, _ in enumerate(deck):
+        for j in range(i):
+            deck[i], deck[j] = deck[j], deck[i]
+            yield deck.copy()
+            deck[i], deck[j] = deck[j], deck[i]
+
+
+def local_maximum(start, func):
+    """Find a local maximum for a function on permutations"""
+    maximum = func(start)
+    new = max(mini_shuffle(start), key=func, default=start)
+    if func(new) <= maximum:
+        return start
+    return local_maximum(new, func)
+
+
+def prob_max_local(num):
+    """Generate random shuffles, then locally maximize nshuffles"""
+    max_shuffles = -1
+    deck = list(range(1, num + 1))
+    while True:
+        shuffle(deck)
+        deck = local_maximum(deck, nshuffles)
+        if nshuffles(deck) > max_shuffles:
+            max_shuffles = nshuffles(deck)
+            print(deck, max_shuffles)
